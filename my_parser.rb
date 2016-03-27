8cb44b42c1fe7f3942adeb43f1905942dacd8771
Federico <fed.x.gibson@gmail.com>
2016-03-27 19:52:19 -0300

    itemize

--- my_parser.rb
+++ my_parser.rb
@@ -2,7 +2,7 @@ require 'date'
 
 class MyParser
   def parse
-    @full_text = ""
+    @item = ""
     @lines = []
     @columns = [0, 5, 26, 37, 52]
 
@@ -10,9 +10,9 @@ class MyParser
     File.open("./flights", "r").each_line.with_index do |line, index|
       # puts index
       @lines.push(line)
-      @full_text+= line
+      @item+= line
     end
-    # puts full_text
+    # puts item
     # al_flt_clss_meal = lines[0]
     extract_data split_by_item split_by_date
   end
@@ -30,7 +30,7 @@ class MyParser
   #   end
   #
   #   if number_of_dates < 0
-  #     return [@full_text]
+  #     return [@item]
   #   end
   #
   #   date_indexes.push(@lines.length)
@@ -43,7 +43,7 @@ class MyParser
 
   def split_by_date
     #scans for date + items pattern until next date
-    return @full_text.scan(/((?:\d+ \D{3} \d+)?(?:.+?(?=\d+ \D{3} \d+)|.+))/m)
+    return @item.scan(/((?:\d+ \D{3} \d+)?(?:.+?(?=\d+ \D{3} \d+)|.+))/m)
   end
 
   def split_by_item dates_with_items
@@ -53,43 +53,53 @@ class MyParser
     dates_with_items.each do |date_item|
       date_with_item_object = {}
       date = /(\d+ \D{3} \d+)(.+)/.match(date_item[0])
-      items = /(?:\d+ \D{3} \d+)?(.+)/m.match(date_item[0])[1]
-      date_with_item_object[:date] =  date[0] unless date.nil?
+      items = /(?:\d+ \D{3} \d+\s+?-\s+?\w+)?(.+)/m.match(date_item[0])[1]
+      date_with_item_object[:date] =  Date.parse(date[1]) unless date.nil?
       date_with_item_object[:items] = itemize items
       dates_with_items_formated.push(date_with_item_object)
     end
-    puts dates_with_items_formated
     return dates_with_items_formated
   end
 
   def itemize text
-    #separar por tipo,
+    result = text.strip.scan(/(?:\s+)(AIR|HOTEL|CAR)((.+?)(?=\n+(?:\s+)(?:AIR|HOTEL|CAR))|.+)/m)
+    item_objects = []
+    result.each do |parsed_item|
+      item_object = {
+        type: parsed_item[0],
+        item: parsed_item[1]
+      }
+      item_objects.push(item_object)
+    end
+    return item_objects
   end
 
   def extract_data dates_with_items
-
+    parsed_items = []
+    puts dates_with_items
     dates_with_items.each do |date_with_item|
-      puts get_data_type date_with_item
-    #   case
-    #   when 'AIR'
-    #
-    #   when 'CAR'
-    #   when 'HOTEL'
-    #   end
+      date_with_item[:items].each do |item|
+        case item[:type]
+        when 'AIR'
+          parse_air date_with_item[:date], item[:item]
+        when 'CAR'
+          # parse_car date_with_item[:date] item
+        when 'HOTEL'
+          # parse_hotel date_with_item[:date] item
+        else
+          0
+        end
+      end
     end
-
+    return 0
   end
 
-  def get_data_type data
-    return /(AIR|HOTEL|CAR)\s{3}/.match(data[:items])[1]
-  end
-
-  def extract_air
+  def parse_air(date, item)
       regexps = {
         each_data:  /\s{2,}/,
         confirmation_number: /REF: (.*)\n/,
         date: /(\d+) (\D{3}) (\d+)/,
-        type: /(AIR|HOTEL|CAR)\s{3}/,
+        type: /^(AIR|HOTEL|CAR)/,
         from: /LV ((\w+\s?)+)\s{2,}/,
         to: /AR ((\w+\s?)+)\s{2,}/,
         aircraft_type: /EQP: (.*)\n/,
@@ -100,28 +110,31 @@ class MyParser
         flight_number: /FLT:(\d+)/,
         arrive: /AR.*?(\d+P)/
       }
-
-      @parsed[:type] = regexps[:type].match(full_text)[1]
-      @parsed[:airline] = al_flt_clss_meal[(@columns[1]..@columns[2])].rstrip
-      @parsed[:flight_number] = regexps[:flight_number].match(full_text)[1]
-      @parsed[:class] = al_flt_clss_meal[(@columns[3]..@columns[4])].rstrip
-      @parsed[:meal] = al_flt_clss_meal[@columns[4]..-2]
-      @parsed[:operated_by] = regexps[:operated_by].match(full_text)[1]
-      @parsed[:from] = regexps[:from].match(full_text)[1]
-      @parsed[:aircraft_type] = regexps[:aircraft_type].match(full_text)[1]
-      @parsed[:depart] = regexps[:depart].match(full_text)[1]
-      @parsed[:duration] = regexps[:duration].match(full_text)[0]
-      @parsed[:to] = regexps[:to].match(full_text)[1].rstrip
-      @parsed[:stops] = regexps[:stops].match(full_text)[0] == 'NON-STOP'?0:1
-      @parsed[:arrive] = regexps[:arrive].match(full_text)[1]
-      @parsed[:confirmation_number] = regexps[:confirmation_number].match(full_text)[1]
+      puts item
+      item = item.strip
+      # @parsed[:type] = regexps[:type].match(item)[1]
+      # @parsed[:airline] = al_flt_clss_meal[(@columns[1]..@columns[2])].rstrip
+      # @parsed[:flight_number] = regexps[:flight_number].match(item)[1]
+      # @parsed[:class] = al_flt_clss_meal[(@columns[3]..@columns[4])].rstrip
+      # @parsed[:meal] = al_flt_clss_meal[@columns[4]..-2]
+      # @parsed[:operated_by] = regexps[:operated_by].match(item)[1]
+      # @parsed[:from] = regexps[:from].match(item)[1]
+      # @parsed[:aircraft_type] = regexps[:aircraft_type].match(item)[1]
+      # @parsed[:depart] = regexps[:depart].match(item)[1]
+      # @parsed[:duration] = regexps[:duration].match(item)[0]
+      # @parsed[:to] = regexps[:to].match(item)[1].rstrip
+      # @parsed[:stops] = regexps[:stops].match(item)[0] == 'NON-STOP'?0:1
+      # @parsed[:arrive] = regexps[:arrive].match(item)[1]
+      # @parsed[:confirmation_number] = regexps[:confirmation_number].match(item)[1]
 
       return @parsed
   end
 
-  def extract_car
+  def parse_car(date, item)
+    return {}
   end
 
-  def extract_hotel
+  def parse_hotel(date, item)
+    return {}
   end
 end
